c6bf78ace589061edba3043d582605b3e9b82bab
Scorabeyko <satlovdanijl@gmail.com>
2026-02-01 21:03:30 +0400

    Update: update details

--- src/widgets/details/Details.jsx
+++ src/widgets/details/Details.jsx
@@ -2,9 +2,9 @@ import { memo, useMemo } from "react"
 import styles from './Details.module.scss'
 
 export const Details = (props) => {
-    const { note } = props 
+    const { note } = props
 
-    let Tag = useMemo(() => {
+    const tag = useMemo(() => {
         if(!note?.Tag) return ''
 
         return note.Tag.startsWith('#') 
@@ -13,22 +13,31 @@ export const Details = (props) => {
     }, [note.Tag])
 
     return (
-        <>
         <div className={styles.container}>
             <div className={styles.title}>
                 <h2>{note.Word}</h2>
                 <h2>{note.Translate ? note.Translate : 'Empty Translate!'}</h2>
             </div>
             <main className={styles.main}>
-                <a 
-                href={`https://www.google.com/search?q=${encodeURIComponent(Tag)}`}
-                target="_blank"
-                >
-                    {note.Tag}
-                </a>
+                <span className={styles.spanTitle}>{note.Word} - <br/><span className={styles.description}>{note.Description}</span></span>
+                <div className={styles.frame}>
+                    <div className={styles.frameTag}>
+                        {note.Tag 
+                        ?
+                        <a
+                        className={styles.link}
+                        href={`https://www.google.com/search?q=What+is+${encodeURIComponent(tag)}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                            {note.Tag}
+                        </a> 
+                        :
+                        <span>Empty tag</span>}
+                    </div>
+                </div>
             </main>
         </div>
-        </>
     )
 }
 
